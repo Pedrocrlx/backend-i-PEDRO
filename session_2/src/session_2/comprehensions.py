a6d8ced207a796509968cd28781ee7724d6aa3d9
Pedro Santos <pedrosantoscrlx@gmail.com>
2025-02-28 22:45:12 +0000

    session_2 ✅

---
+++ session_2/src/session_2/comprehensions.py
@@ -0,0 +1,4 @@
+def main():
+    print("pedro na percebe disto")
+if __name__ == "__main__":
+    main()
---
+++ session_2/src/session_2/functions.py
@@ -0,0 +1,17 @@
+# def sum(*args):
+#     result = 0
+#     for arg in args:
+#         result += arg
+#     return result
+
+
+# print("function that accepts any number of numeric arguments (using *args)", sum(1, 2, 3))
+
+
+# def filterValues(**kwargs):
+
+#     if len(kwargs.values()) < 2:
+#         raise Exception("Sorry, no numbers below 1")
+#     joreg = "tudo certo"
+#     return joreg
+# print(filterValues(b="banana"))
