3c6a45a4bcc93d2246aea78c2ce94029039dd86b
Pedro Santos <pedrosantoscrlx@gmail.com>
2025-03-18 22:06:55 +0000

    update

--- session_12/src/session_12/main.py
+++ session_12/src/session_12/main.py
@@ -1,26 +1,14 @@
+from src.session_12.models import Task
 from fastapi import FastAPI
-from sqlmodel import SQLModel, create_engine
-import os
 
 api = FastAPI(
     title="TODO Etic_Algarve API")
-
-DB_USER = os.getenv("DB_USER", None)
-DB_PASS = os.getenv("DB_PASS", None)
-DB_HOST = os.getenv("DB_HOST", None)
-DB_PORT = os.getenv("DB_PORT", None)
-DB_NAME = os.getenv("DB_NAME", None)
-
-engine = create_engine(
-    f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
-
-SQLModel.metadata.create_all(engine)
 
 @api.post("/task")
 def create_task():
     pass
 
-@api.get("/task")
+@api.get("/task", response_model=Task)
 def list_task():
     pass
 
@@ -31,6 +19,7 @@ def edit_task():
 @api.patch("/task")
 def close_task():
     pass
+
 
 @api.delete("/task")
 def delete_task():
--- session_12/src/session_12/models.py
+++ session_12/src/session_12/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+import os
 from typing import Optional
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, create_engine
 
 
 class Task(SQLModel, table=True):
@@ -9,3 +10,15 @@ class Task(SQLModel, table=True):
     description: str
     due_date: datetime
     is_done: bool = False
+
+
+DB_USER = os.getenv("DB_USER", None)
+DB_PASS = os.getenv("DB_PASS", None)
+DB_HOST = os.getenv("DB_HOST", None)
+DB_PORT = os.getenv("DB_PORT", None)
+DB_NAME = os.getenv("DB_NAME", None)
+
+engine = create_engine(
+    f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+SQLModel.metadata.create_all(engine)
