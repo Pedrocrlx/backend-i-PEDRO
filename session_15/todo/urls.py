03781b1b47f29a56c923b9c2faf594b47efbc8e7
Pedro Santos <pedrosantoscrlx@gmail.com>
2025-03-21 22:03:49 +0000

    session_15: done

--- session_15/todo/urls.py
+++ session_15/todo/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.urls import path
 from todo import views
-
+from django.contrib.auth.views import LoginView
 urlpatterns = [
-    path("tasks", views.TaskListView.as_view(), name="index"),
+    path("tasks", views.TaskListView.as_view(), name="task_list"),
+    path("signup", views.SignupView.as_view(), name="signup"),
+    path("signin", LoginView.as_view(), name="signin"),
+    path("", views.IndexView.as_view(), name="index"),
 ]
--- session_15/todo/views.py
+++ session_15/todo/views.py
@@ -1,17 +1,36 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView,TemplateView
+from django.views.generic import ListView, TemplateView, FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from todo.models import Task
 
 # Create your views here.
+
+
 def index(request):
     tasks = Task.objects.all()
-    return render(request,"todo/index.html", {"foo":"World", "tasks":tasks})
+    return render(request, "todo/index.html", {"foo": "World", "tasks": tasks})
 
-class TaskListView(ListView):
+
+class TaskListView(LoginRequiredMixin, ListView):
+    login_url = "/signin"
     model = Task
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user).all()
+
 
 class IndexView(TemplateView):
     http_method_names = ['get']
     template_name = "todo/index.html"
+
+
+class SignupView(FormView):
+    template_name = "registration/signup.html"
+    success_url = "/"
+    form_class = UserCreationForm
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
